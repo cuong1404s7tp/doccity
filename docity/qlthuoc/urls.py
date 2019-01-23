9b46f44f180ee416277ce42f03e7e6f65580a843
docity <cuong2177@gmail.com>
2019-01-22 22:54:34 -0800

    docity site, first commit

---
+++ docity/qlthuoc/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from ecom import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^product/(?P<product_id>[0-9]+)/$', views.product, name='product'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
